7e539abb11d346e0d0a540ab008c89e21a05ce7e
Vinicius Foss <viniciusaug.foss@gmail.com>
2025-06-11 00:31:34 -0300

    added changes

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "aceternity.com",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.aceternity.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
